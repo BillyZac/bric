6ebf2e4abffaa21b109dabec9c5c51b413fedb6f
Zachary Smith <billyzacsmith@gmail.com>
2015-12-24 17:40:14 -0700

    Add random specks of color to the brick. Each speck is 10px by 10px. This is ased on an assumption that a visible pixel is 10px by 10px. Of course, this might not fit with the aesthetics of the game, but it can be easily changed by adjusting the "grainDimension" variable.
    The specks are placed on an imaginary 10px grid, in keeping with the pixel art aesthetic.

--- brick.js
+++ brick.js
@@ -3,19 +3,6 @@ module.exports = brick
 var Canvas = require('canvas')
 var fs = require('fs')
 
-var palette = [
-  'rgb(146, 143, 144)', // hsla(340, 2, 57, 1)
-  'rgb( 39,  33,  58)', // hsla(254, 43, 23, 1)
-  'rgb( 62,  52,  73)', // hsla(269, 29, 29, 1)
-  'rgb( 67,  52,  73)', // hsla(283, 29, 29, 1)
-  'rgb(100,  85,  95)', // hsla(320, 15, 39, 1)
-  'rgb(137, 113, 144)', // hsla(286, 22, 56, 1)
-  'rgb(146, 133, 132)', // hsla(4, 10, 57, 1)
-  'rgb( 73,  59,  18)', // hsla(45, 75, 29, 1)
-  'rgb( 81,  43,  32)', // hsla(13, 60, 32, 1)
-  'rgb(111,  91,  38)'  // hsla(44, 66, 44, 1)
-]
-
 var paletteHSLA = [
   'hsla(340, 2%, 57%, 1)',
   'hsla(254, 43%, 23%, 1)',
@@ -48,6 +35,31 @@ function brick(width, height) {
     // By making this one 2px smaller than the border color, the brick gets a border.
     ctx.rect(2, 2, width-4, height-4);
     ctx.fill();
+
+    addNoise(width, height)
+
     fs.writeFile('./bricks/brick-' + width + 'px-' + height + 'px-' + paletteHSLA[i] + '.png', canvas.toBuffer());
   }
+}
+
+function addNoise(width, height) {
+  for (var i=0; i<50; i++) {
+    // Each noise grain is 10px by 10px
+    var grainDimension = 10
+    // Find a random spot on the canvas.
+    // Make sure it's on a 10px grid, i.e. x = 60, not x = 62
+    var x = Math.floor(Math.random() * width / grainDimension) * grainDimension
+    var y = Math.floor(Math.random() * height / grainDimension) * grainDimension
+    // Choose a color at random from our palette
+    var paletteIndex = Math.floor(Math.random() * paletteHSLA.length)
+    ctx.fillStyle = paletteHSLA[paletteIndex]
+    // Tone down the opacity of the grain
+    ctx.globalAlpha = 0.15
+    ctx.beginPath()
+    ctx.rect(x, y, grainDimension, grainDimension)
+    ctx.fill()
+  }
+  // Reset the opacity.
+  // This isn't necessary as long as the palette specifies in hsla, but just to be on the safe side.
+  ctx.globalAlpha = 0.15
 }
